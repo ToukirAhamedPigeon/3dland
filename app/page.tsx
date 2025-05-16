aa85721e723550d34b482a7e0d58f87a58de9f59
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-05-16 16:16:01 +0600

    All

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button variant="destructive">Click me</Button>
+      <Button className="bg-blue-600 text-light hover:bg-blue-500"> <Link href="/gsap">GSAP</Link></Button>
     </div>
   );
 }
