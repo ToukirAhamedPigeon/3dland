251b1b91044ef56e52b5df0d2b91e266f4bc40c9
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-05-19 21:26:32 +0600

    Hero Section

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,12 +10,12 @@ const Navbar = () => {
         <Image src={appleImg} alt="apple" width={14} height={18}/>
         <div className="flex flex-1 justify-center max-sm:hidden">
             {navLists.map((nav)=>(
-                <div key={nav}>
+                <div key={nav} className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>
                     {nav}
                 </div>
             ))}
         </div>
-        <div>
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
             <Image src={searchImg} alt="search" width={18} height={18}/>
             <Image src={bagImg} alt="search" width={18} height={18}/>
         </div>
